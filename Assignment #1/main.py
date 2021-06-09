5f8cd5b9777b45c9748f20c9503cc614e2ebd331
Mateus Metzker <metzkerrr@gmail.com>
2021-06-09 18:58:42 -0300

    Melhorias no item 1 e código do item 2 e 3

--- Assignment #1/main.py
+++ Assignment #1/main.py
@@ -24,7 +24,8 @@ def menu(url):
     header = "-" * 120
     lines = [header, "{0:<25s} {1}".format('Opção', 'Descrição'), header,
                 "{0:<25s} {1}".format('1', 'Listar os tópicos do índice do artigo'), "{0:<25s} {1}".format('2', 'Listar todos os nomes de arquivos de imagens presentes no artigo'),
-                "{0:<25s} {1}".format('3', 'Listar todas as referências bibliográficas disponíveis na página'), "{0:<25s} {1}".format('4', 'Listar todos os links para outrosartigos da Wikipédia que são citados no conteúdo do artigo'), header]
+                "{0:<25s} {1}".format('3', 'Listar todas as referências bibliográficas disponíveis na página'), "{0:<25s} {1}".format('4', 'Listar todos os links para outros artigos da Wikipédia que são citados no conteúdo do artigo'),
+                "{0:<25s} {1}".format('5', 'Sair'), header]
     print("\n".join(lines))
     
     op = input("Digite uma opção: ")
@@ -39,7 +40,7 @@ def menu(url):
         print("Erro no servidor.")
         sys.exit()
     
-    # Lê o html e procura pela tag MAIN
+    # Lê o html
     res = BeautifulSoup(html.read(),"html5lib");
 
     # Listar tópicos do índice
@@ -49,6 +50,12 @@ def menu(url):
 
         # Procura a tag
         content = res.findAll(attrs = { 'class' : re.compile('toclevel-\d+ tocsection-\d+') })
+
+        # Verifica se o artigo tem índice
+        if content == []:
+            print("Este artigo não tem índice.")
+            time.sleep(3)
+            sys.exit()
 
         # Printa o índice
         header = "-" * 40
@@ -61,12 +68,79 @@ def menu(url):
                 print("\t" * occ + "{0:>2s} {1}".format(num.group().replace('>', '').replace('<', '.'), text.group().replace('toctext">', '')))
             else:  
                 print("{0:>2s} {1}".format(num.group().replace('>', '').replace('<', '.'), text.group().replace('toctext">', '')))
+
+        # Volta ao menu
+        while True:
+            print("\n\nPressione ENTER para voltar ao menu...")
+            op = input("")
+            menu(url)
+
     elif op == '2':
-        pass
+        # Limpa a tela
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        # Procura a tag
+        content = res.findAll(attrs = { 'href' : re.compile('\/[a-zA-Z]+\/[a-zA-Z]+:[a-zA-Z0-9\_\-\,]+\.[a-zA-Z]+') })
+
+        # Verifica se o artigo tem imagens
+        if content == []:
+            print("Este artigo não tem imagens.")
+            time.sleep(3)
+            sys.exit()
+
+        # Printa as imagens
+        header = "-" * 40
+        print(header +  "\n{0:^40s}".format("Imagens") + '\n' + header)
+        images = []
+        for ind in content:
+            text = re.search('\/[a-zA-Z]+\/[a-zA-Z]+:[a-zA-Z0-9\_\-\,]+\.[a-zA-Z]+', str(ind))
+            
+            # Tratamento para não printar repetidos
+            if text.group() not in images:
+                images.append(text.group())
+                if ':' in os.path.basename(text.group()):
+                    print(os.path.basename(text.group()).split(':')[1])
+                else:
+                    print(os.path.basename(text.group()))
+
+        # Volta ao menu
+        while True:
+            print("\n\nPressione ENTER para voltar ao menu...")
+            op = input("")
+            menu(url)
+
     elif op == '3':
-        pass
+        # Limpa a tela
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        # Procura a tag
+        content = res.findAll(attrs = { 'id' : re.compile('cite_note-\d+') })
+
+        # Verifica se o artigo tem referências
+        if content == []:
+            print("Este artigo não tem referências.")
+            time.sleep(3)
+            sys.exit()  
+
+        # Printa as referências
+        header = "-" * 40
+        print(header +  "\n{0:^40s}".format("Imagens") + '\n' + header)
+        num = 1  #  índice das referências
+        for ind in content:
+            text = re.search('<span class="reference-text"><.+>?[a-zA-Z0-9À-ú]+>', str(ind))
+            print(str(num) + ' ' + text.group()) #TODO: Parei aqui
+            num+=1
+
+        # Volta ao menu
+        while True:
+            print("\n\nPressione ENTER para voltar ao menu...")
+            op = input("")
+            menu(url)    
+
     elif op == '4':
         pass
+    elif op == '5':
+        sys.exit()
     else:
         # Limpa a tela
         os.system('cls' if os.name == 'nt' else 'clear')
